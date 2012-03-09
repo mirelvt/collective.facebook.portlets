ed70ec51d2fe7539aa73d1af06076c4ffadd0be6
hvelarde <hector.velarde@gmail.com>
2012-03-09 20:46:44 -0300

    fixed i18n and updated Spanish translation

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,14 +1,30 @@
 #! /bin/sh
 
+PATH=bin:$PATH
 I18NDOMAIN="collective.facebook.portlets"
+BASE_DIRECTORY="src/collective/facebook/portlets"
 
 # Synchronise the templates and scripts with the .pot.
-# All on one line normally:
-i18ndude rebuild-pot --pot src/collective/facebook/portlets/locales/${I18NDOMAIN}.pot \
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
-    src/collective/facebook/portlets
+    ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
-for po in src/collective/facebook/portlets/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    i18ndude sync --pot src/collective/facebook/portlets/locales/${I18NDOMAIN}.pot $po
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
+
+# Synchronise the templates and scripts with the .pot.
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
+    --create plone \
+    ${BASE_DIRECTORY}/configure.zcml \
+    ${BASE_DIRECTORY}/profiles/default
+
+# Synchronise the plone's pot file (Used for the workflows)
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
+done
+
+# Report of errors and suspect untranslated messages
+i18ndude find-untranslated ${BASE_DIRECTORY}
