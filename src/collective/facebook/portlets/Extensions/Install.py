47206493f4c98325911f30846e33ce4d16475e13
Franco Pellegrini <frapell@gmail.com>
2012-05-23 11:16:24 -0300

    Replacing the way we know if we should reinstall or not our initial profile. Also added tests for it

--- src/collective/facebook/portlets/Extensions/Install.py
+++ src/collective/facebook/portlets/Extensions/Install.py
@@ -1,17 +1,5 @@
 from Products.CMFCore.utils import getToolByName
 
-
-def install(portal, reinstall=False):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    if not reinstall:
-        setup_tool.runAllImportStepsFromProfile(
-                                'profile-collective.facebook.portlets:initial')
-
-    setup_tool.runAllImportStepsFromProfile(
-                                'profile-collective.facebook.portlets:default')
-    return "Ran all uninstall steps."
-
-
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = getToolByName(portal, 'portal_setup')
